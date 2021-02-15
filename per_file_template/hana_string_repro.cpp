fa2a8a130564cdaefb40e250214ebddd1f2cf39e
Billy Donahue <billy.donahue@gmail.com>
2021-02-15 02:37:30 -0500

    show fail and pass

--- per_file_template/hana_string_repro.cpp
+++ per_file_template/hana_string_repro.cpp
@@ -6,18 +6,26 @@ void print(T&& t) {
     printf(t.c_str());
 }
 
-#ifdef SHOW_ERROR
-#define INNER_ARG auto
-#else
-#define INNER_ARG int
-#endif
-
-int main() {
+void ok() {
     [](auto) {
-        [](INNER_ARG) {
+        [](int) {
             auto str = BOOST_HANA_STRING("xxx");
             printf("%s\n", str.c_str());
         }(2);
     }(1);
+}
+
+void err() {
+    [](auto) {
+        [](auto) {
+            auto str = BOOST_HANA_STRING("xxx");
+            printf("%s\n", str.c_str());
+        }(2);
+    }(1);
+}
+
+int main() {
+    ok();
+    err();
     return 0;
 }
