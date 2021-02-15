28af28c8ed2d95ea11d29aa0a365cf72ca2ae5da
Billy Donahue <billy.donahue@gmail.com>
2021-02-15 01:34:56 -0500

    minimal

--- per_file_template/tiny_test.cpp
+++ per_file_template/tiny_test.cpp
@@ -2,16 +2,8 @@
 #include <iostream>
 #include <utility>
 
-template <typename T>
-void dump(const T& t) {
-    std::cout << "[" << __PRETTY_FUNCTION__ << "] t=" << t.c_str() << std::endl;
-}
-
 template <char...Cs>
-struct String {
-    static constexpr const char storage[] = {Cs..., 0};
-    constexpr const char* c_str() const { return storage; }
-};
+struct String {};
 
 template <typename S, size_t...Is>
 constexpr auto prepare_(S s, std::index_sequence<Is...>) {
@@ -24,26 +16,21 @@ auto prepare(S s) {
     return prepare_(s, std::make_index_sequence<sizeof(S::get())-1>{});
 }
 
-#define MACRO(s) ( \
-    prepare([]() -> decltype(auto) { \
-        struct Tmp { \
-            static constexpr decltype(auto) get() { return s; } \
-        }; \
-        return Tmp{}; \
-    }()))
-
-
 /*
  * Two LOGV2 statements in the Numeric test below showed unexplained build failures.
  * Must be related to the nested nature of the lambdas.
  */
 void TestBugWithStringPackEvaluation() {
-    [[maybe_unused]] auto outer = [](auto a) {
+    auto outer = [](auto a) {
         auto inner = [](auto b) {
-            auto str = MACRO("hi");
-            std::cout << str.c_str() << "\n";
+            auto str = prepare([]() -> decltype(auto) {
+                struct Tmp {
+                    static constexpr decltype(auto) get() { return "hi"; }
+                };
+                return Tmp{};
+            }());
         };
-        inner(a);
+        inner(111);
     };
     outer(222);
 }
