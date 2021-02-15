0202c8b28f8929782d2b41bcac0388dbb0024189
Billy Donahue <billy.donahue@gmail.com>
2021-02-15 01:57:34 -0500

    minimize test

--- per_file_template/tiny_test.cpp
+++ per_file_template/tiny_test.cpp
@@ -1,38 +1,18 @@
 
-#include <iostream>
-#include <utility>
+template <typename T>
+void print(T&&) {}
 
-template <char...Cs>
-struct String {};
-
-template <typename S, size_t...Is>
-constexpr auto prepare_(S s, std::index_sequence<Is...>) {
-    return String<S::get()[Is]...>{};
-}
-
-template <typename S>
-auto prepare(S s) {
-    std::cout << "[" << __PRETTY_FUNCTION__ << "]" << std::endl;
-    return prepare_(s, std::make_index_sequence<sizeof(S::get())-1>{});
-}
-
-/*
- * Two LOGV2 statements in the Numeric test below showed unexplained build failures.
- * Must be related to the nested nature of the lambdas.
- */
 void TestBugWithStringPackEvaluation() {
-    auto outer = [](auto a) {
-        auto inner = [](auto b) {
-            auto str = prepare([]() -> decltype(auto) {
+    [](auto) {
+        [](auto) {
+            print([]() -> decltype(auto) {
                 struct Tmp {
-                    static constexpr decltype(auto) get() { return "hi"; }
+                    static constexpr decltype(auto) get() { return 123; }
                 };
                 return Tmp{};
             }());
-        };
-        inner(111);
-    };
-    outer(222);
+        }(2);
+    }(1);
 }
 
 
